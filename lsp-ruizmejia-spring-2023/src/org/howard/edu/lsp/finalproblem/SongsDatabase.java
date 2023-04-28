d186d1de223ce00c990d675303906acfed46fcb7
Jose Ruiz <josemanuelruizmejia.com>
2023-04-27 19:59:57 -0500

    Again, trying my best. Final submission.

--- lsp-ruizmejia-spring-2023/src/org/howard/edu/lsp/finalproblem/SongsDatabase.java
+++ lsp-ruizmejia-spring-2023/src/org/howard/edu/lsp/finalproblem/SongsDatabase.java
@@ -7,9 +7,9 @@ import java.util.Set;
 
 public class SongsDatabase {
     // Key is the genre
-	// HashSet contains associated songs 
+    // HashSet contains associated songs 
     private Map<String, HashSet<String>> map = new HashMap<String, HashSet<String>>();
-    
+
     // Adds a song title to a genre 
     public void addSong(String genre, String songTitle) {
         if (!map.containsKey(genre)) {
@@ -17,8 +17,8 @@ public class SongsDatabase {
         }
         map.get(genre).add(songTitle);
     }
-    
-     //Returns all songs for a genre in a Set
+
+    // Returns all songs for a genre in a Set
     public Set<String> getSongs(String genre) {
         if (map.containsKey(genre)) {
             return map.get(genre);
@@ -26,8 +26,8 @@ public class SongsDatabase {
             return new HashSet<String>();
         }
     }
-    
-    // Returns a genre of the given songs title
+
+    // Returns a genre of the given song title
     public String getGenreOfSong(String songTitle) {
         for (Map.Entry<String, HashSet<String>> entry : map.entrySet()) {
             String genre = entry.getKey();
@@ -38,5 +38,15 @@ public class SongsDatabase {
         }
         return null;
     }
-}
 
+    public static void main(String[] args) {
+        SongsDatabase db = new SongsDatabase();
+        db.addSong("Rap", "Savage");
+        db.addSong("Rap", "Gin and Juice");
+        db.addSong("Jazz", "Always There");
+        @SuppressWarnings("unused")
+		Set<String> songs = db.getSongs("Rap"); // contains Savage and Gin and Juice
+        System.out.println(db.getGenreOfSong("Savage")); // prints "Rap"
+        System.out.println(db.getGenreOfSong("Always There")); // prints "Jazz"
+    }
+}
--- lsp-ruizmejia-spring-2023/src/org/howard/edu/lsp/finalproblem/SongsDatabaseTest.java
+++ lsp-ruizmejia-spring-2023/src/org/howard/edu/lsp/finalproblem/SongsDatabaseTest.java
@@ -1,21 +1,18 @@
 package org.howard.edu.lsp.finalproblem;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Set;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class SongsDatabaseTest {
+import java.util.HashSet;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class SongsDatabaseTest {
     private SongsDatabase db;
 
     @BeforeEach
-    //This adds the songs into database for testing.
-    public void setUp() {
+    void setUp() {
         db = new SongsDatabase();
         db.addSong("Rap", "Savage");
         db.addSong("Rap", "Gin and Juice");
@@ -23,37 +20,26 @@ public class SongsDatabaseTest {
     }
 
     @Test
-    // adds a new song and tests if the song is added to the database.
-    public void testAddSong() {
-        db.addSong("Salsa", "Azuca");
-        Set<String> songs = db.getSongs("Salsa");
-        assertEquals(1, songs.size());
-        assertTrue(songs.contains("Azuca"));
+    void testAddSong() {
+        db.addSong("Pop", "Shallow");
+        Set<String> expected = new HashSet<>();
+        expected.add("Shallow");
+        assertEquals(expected, db.getSongs("Pop"));
     }
 
     @Test
-    //gets songs from database based on genre.
-    // Tests song title.
-    public void testGetSongs() {
-        Set<String> rapSongs = db.getSongs("Rap");
-        Set<String> jazzSongs = db.getSongs("Jazz");
-        assertEquals(2, rapSongs.size());
-        assertTrue(rapSongs.contains("Savage"));
-        assertTrue(rapSongs.contains("Gin and Juice"));
-        assertEquals(1, jazzSongs.size());
-        assertTrue(jazzSongs.contains("Always There"));
+    void testGetSongs() {
+        Set<String> expected = new HashSet<>();
+        expected.add("Savage");
+        expected.add("Gin and Juice");
+        assertEquals(expected, db.getSongs("Rap"));
     }
 
     @Test
-    // Gets songs from databse based on Title.
-    //Test if genre matches.
-    public void testGetGenreOfSong() {
-        String genre1 = db.getGenreOfSong("Savage");
-        String genre2 = db.getGenreOfSong("Always There");
-        String genre3 = db.getGenreOfSong("Azuca");
-        assertEquals("Rap", genre1);
-        assertEquals("Jazz", genre2);
-        assertNull(genre3);
+    void testGetGenreOfSong() {
+        assertEquals("Rap", db.getGenreOfSong("Savage"));
+        assertEquals("Jazz", db.getGenreOfSong("Always There"));
+        assertNull(db.getGenreOfSong("Smooth"));
     }
 }
 
